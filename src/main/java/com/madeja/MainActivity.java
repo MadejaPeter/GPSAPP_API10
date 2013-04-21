37a6a3d23cb6d398fb30be5282f12c0364be13dc
Peter Madeja <madeja.fei@gmail.com>
2013-04-21 21:57:01 +0200

    Pridanie funcionality vyhladavania miest v mapach

--- src/main/java/com/madeja/MainActivity.java
+++ src/main/java/com/madeja/MainActivity.java
@@ -1,12 +1,20 @@
 package com.madeja;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.google.android.maps.GeoPoint;
@@ -60,6 +68,45 @@ public class MainActivity extends MapActivity {
         mapView.getController().animateTo(new GeoPoint((int)(loc.getLatitude() * 1E6), (int)(loc.getLongitude() * 1E6)));
         Log.i("I", "Location" + loc.toString());
         mapView.getController().setZoom(15);
+        
+        final Button searchButton = (Button)findViewById(R.id.searchbutton);
+        searchButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			    // TODO Auto-generated method stub
+			    if (v == searchButton) {
+			        Geocoder geo = new Geocoder(getApplicationContext(), Locale.getDefault());
+			        try {
+			        	EditText mapSearchBox = (EditText) findViewById(R.id.search);
+			        	
+			            List<Address> addresses = geo.getFromLocationName(mapSearchBox.getText().toString(), 5);
+			            if (addresses.size() > 0) {
+			                GeoPoint p = new GeoPoint((int) (addresses.get(0)
+			                        .getLatitude() * 1E6), (int) (addresses.get(0)
+			                        .getLongitude() * 1E6));
+
+			                controller.animateTo(p);
+			                controller.setZoom(12);
+//			                MapOverlay mapOverlay = new MapOverlay();
+//			                List<Overlay> lisOverlays = mapView.getOverlays();
+//			                lisOverlays.clear();
+//			                lisOverlays.add(mapOverlay);
+
+			            } else {
+			                AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+			                adb.setTitle("Google Map");
+			                adb.setMessage("please provide proper place");
+			                adb.setPositiveButton("Close", null);
+			                adb.show();
+			            }
+			        } catch (IOException e) {
+			            // TODO Auto-generated catch block
+			            e.printStackTrace();
+			        }
+			    }
+			}
+		});
     }
 
     @Override
